1c7a5809aac81ba21713b2421b685dd4db44c34d
dm9pZCAq <v@0x0c.xyz>
2021-09-06 20:14:30 +0300

    config.h: some fixes, rename interval macroses

--- config.h
+++ config.h
@@ -60,12 +60,13 @@
  *   - components_config.h
  */
 
-
 /* for usleep */
-#define SEC * 1000
-#define MIN * 60 SEC
+#define _SEC *1000
+#define _MIN *(60 _SEC)
+#define _HR  *(60 _MIN)
 
-#define ONCE ((unsigned int) -1)  /* to run */
+/* interval to run only once */
+#define ONCE ((unsigned int)-1)
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -77,9 +78,9 @@ static const char unknown_str[] = "n/a";
  * then change `BUFF_SZ` in util.h
  */
 
-static const char IFC[] = "wlan0";  /* wifi interface */
+#define IFC "wlan0" /* wifi interface */
 
-
+/* clang-format off */
 static struct arg_t args[] = {
 
 /* function		format		argument	interval (in ms) */
@@ -87,19 +88,20 @@ static struct arg_t args[] = {
 #if USE_X
 /* { bspwm_ws,		" [ %s ]%%{r}",	NULL,		0,	END }, */
 #endif
-{ disk_perc,		" [# %s%%",	"/",		25 SEC,	END },
-{ disk_free,		" %s]",		"/",		25 SEC,	END },
-{ cpu_perc,		"-[ %3s%%]",	NULL,		 1 SEC,	END },
-{ load_avg,		"-[%s]",	NULL,		 3 SEC,	END },
-{ ram_used,		"-[﬙ %s]", 	NULL,		 3 SEC,	END },
-{ vol_perc,		"-[%s]",	NULL,		 0,	END },
-{ wifi_essid,		"-[直 \"%s\"",	IFC,		 2 SEC,	END },
-{ wifi_perc,		" %3s%%]",	IFC,		 2 SEC,	END },
-{ battery_state,	"-[%s",		"BAT0",		 2 SEC,	END },
-{ battery_perc,		" %3s%%]",	"BAT0",		 1 MIN,	END },
-{ datetime,		"-[ %s]",	"%H:%M:%S",	 1 SEC,	END },
+{ disk_perc,		" [# %s%%",	"/",		25 _SEC,	END },
+{ disk_free,		" %s]",		"/",		25 _SEC,	END },
+{ cpu_perc,		"-[ %3s%%]",	NULL,		 1 _SEC,	END },
+{ load_avg,		"-[%s]",	NULL,		 3 _SEC,	END },
+{ ram_used,		"-[﬙ %s]", 	NULL,		 3 _SEC,	END },
+{ vol_perc,		"-[%s]",	NULL,		 0,		END },
+{ wifi_essid,		"-[直 \"%s\"",	IFC,		 2 _SEC,	END },
+{ wifi_perc,		" %3s%%]",	IFC,		 2 _SEC,	END },
+{ battery_state,	"-[%s",		"BAT0",		 2 _SEC,	END },
+{ battery_perc,		" %3s%%]",	"BAT0",		 1 _MIN,	END },
+{ datetime,		"-[ %s]",	"%H:%M:%S",	 1 _SEC,	END },
 #if USE_X && USE_XKB
 { keymap,		"-[ %s] ",	NULL,		 0,	END },
 #endif
 
 };
+/* clang-format on */
