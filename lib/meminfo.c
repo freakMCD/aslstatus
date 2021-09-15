d07058906f23c103cf8f5759fcd8f68ec832cf0e
dm9pZCAq <v@0x0c.xyz>
2021-09-15 23:24:43 +0300

    lib/meminfo.c: use less syscalls and larger buffer

--- lib/meminfo.c
+++ lib/meminfo.c
@@ -6,39 +6,46 @@
 #include "util.h"
 #include "meminfo.h"
 
-/*
- * from linux sources: fs/proc/meminfo.c
- *
- * 16	key: (with spaces)
- * 8	%8lu
- * 4	" kB\n"
- */
-#define LINE_LEN     (16 + 8 + 4)
-#define MEMINFO_LINE (LINE_LEN + LINE_LEN / 2)
+#define READLINE_SIZE BUFSIZ
+
+/* clang-format off */
+#define MYFILE_INIT(FD) { .fd = (FD), .pos = 0 }
+#define MYBUF_INIT      { .pos = 0, .size = 0, .last_nl = 0, .data = {0} }
+/* clang-format on */
 
 struct myfile {
 	int    fd;
 	size_t pos;
 };
+
+struct mybuf {
+	size_t pos;
+	size_t size /* size of valid data */;
+	size_t last_nl /* last '\n' char in 0..size */;
+	char   data[READLINE_SIZE];
+};
+
 enum read_line_status {
 	END = EOF,
 	ERR = 0,
 	OK  = !0,
 };
 
-static enum read_line_status read_line(struct myfile *file,
-				       char	      line[MEMINFO_LINE]);
+static enum read_line_status
+read_line(struct myfile *file, struct mybuf *line_buf, char **line);
 
 uint8_t
 read_meminfo(int fd, const char *key, memory_t *val)
 /* read single field from meminfo */
 {
-	uint8_t	      found = 0;
-	char	      line[MEMINFO_LINE];
-	size_t	      key_len = strlen(key);
-	struct myfile file    = { .fd = fd, .pos = 0 };
+	char *	line;
+	uint8_t found	   = 0;
+	size_t	key_len	   = strlen(key);
 
-	while (read_line(&file, line) == OK) {
+	struct mybuf  buf  = MYBUF_INIT;
+	struct myfile file = MYFILE_INIT(fd);
+
+	while (read_line(&file, &buf, &line) == OK) {
 		if (!strncmp(line, key, key_len)) {
 			SCANF({ return 0; },
 			      1,
@@ -57,11 +64,12 @@ read_meminfo(int fd, const char *key, memory_t *val)
 uint8_t
 read_meminfo_keys(int fd, const struct meminfo_field *info, size_t size)
 {
-	char  line[MEMINFO_LINE];
-	uint8_t readed = 0;
-	struct myfile file = { .fd = fd, .pos = 0 };
+	char *	      line;
+	uint8_t	      readed = 0;
+	struct mybuf  buf    = MYBUF_INIT;
+	struct myfile file   = MYFILE_INIT(fd);
 
-	while (read_line(&file, line) == OK && readed < size) {
+	while (read_line(&file, &buf, &line) == OK && readed < size) {
 		if (!strncmp(line, info[readed].key, info[readed].key_len)) {
 			SCANF({ goto end_while; },
 			      1,
@@ -105,24 +113,43 @@ get_meminfo_swap(int fd, struct meminfo_swap *info)
 }
 
 static inline enum read_line_status
-read_line(struct myfile *file, char line[MEMINFO_LINE])
+read_line(struct myfile *file, struct mybuf *buf, char **line)
 {
-	/*
-	 * TODO
-	 * 
-	 * rewrite with less syscalls and larger buffer
-	 */
-	size_t readed, i;
-	EREAD({ return ERR; }, readed, file->fd, line, MEMINFO_LINE);
+	size_t i;
 
-	if (!readed) return END;
-	for (i = 0; i < readed; i++)
-		if (line[i] == '\n') {
-			line[i] = '\0';
-			file->pos += i + 1;
-			lseek(file->fd, file->pos, SEEK_SET);
-			return OK;
+	if (buf->pos >= buf->last_nl) {
+	reread:
+		buf->pos = !!buf->pos; /* if not first read, skip last '\n' */
+
+		if (!!buf->last_nl)
+			lseek(file->fd,
+			      (file->pos -= (buf->size - buf->last_nl)),
+			      SEEK_SET);
+
+		EREAD({ return ERR; },
+		      buf->size,
+		      file->fd,
+		      WITH_LEN(buf->data));
+
+		if (!buf->size) return END;
+		file->pos += buf->size;
+
+		for (i = 0; i < buf->size; i++)
+			if (buf->data[i] == '\n') buf->last_nl = i;
+
+		*line = buf->data + buf->pos;
+		return OK;
+	} else {
+		for (; buf->pos <= buf->last_nl; buf->pos++) {
+			if (buf->data[buf->pos] == '\n') {
+				/* if at last '\n' try to reread next data */
+				if (buf->pos == buf->last_nl) goto reread;
+
+				*line = buf->data + ++buf->pos;
+				return OK;
+			}
 		}
+	}
 
 	return ERR;
 }
